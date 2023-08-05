ec3182abefa4ac54ec8fbb87144d6a6ba2ac7a0d
sagorluc <mdsagorluc@gmail.com>
2023-08-05 12:58:44 +0600

    update admin panel

--- 07_django_project/01_django_blog_project/main_blog/blog1/admin.py
+++ 07_django_project/01_django_blog_project/main_blog/blog1/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from blog1.models import Post
 
 # Register your models here.
-admin.site.register(Post)
+# admin.site.register(Post)
+
+# modify admin panel
+class BlogModifyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'body')
+    
+admin.site.register(Post, BlogModifyAdmin)
